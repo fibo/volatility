817e4b6ca0be66cc03b061ee5e0697022af88863
fibo <casati_gianluca@yahoo.it>
2018-02-23 19:33:54 +0000

    init repo

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Volatility is the degree of variation of a trading price series over time.
+ *
+ * @param {Array} values
+ * @returns {Number} sigma, a.k.a. *standard deviation* that is the square root of the values *variance*
+ */
+function volatility (values) {
+  const n = values.length
+
+  const mean = values.reduce((a, b) => (a + b), 0) / n
+
+  const deviation = values.reduce((dev, val) => (dev + (val - mean) * (val - mean)), 0)
+
+  return Math.sqrt(deviation / (n - 1))
+}
+
+module.exports = volatility

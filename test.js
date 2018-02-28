18133f658e9a659b2fb40c2e8613c59e49827bdc
fibo <casati_gianluca@yahoo.it>
2018-02-28 21:57:17 +0100

    using descriptive statistic formula

--- test.js
+++ test.js
@@ -4,12 +4,6 @@ const volatility = require('./index')
 const values = [727.7, 1086.5, 1091.0, 1361.3, 1490.5, 1956.1]
 
 const sigma = volatility(values)
-assert.equal(sigma, 420.96248961952256)
+assert.equal(sigma, 384.2844190469114)
 
-console.log(`
-  result is ${sigma}
-
-  See "Sum of squares calculation for female fulmars" example in wikipedia page
-
-  https://en.wikipedia.org/wiki/Standard_deviation
-`)
+console.log(`result is ${sigma}`)

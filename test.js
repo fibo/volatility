7b19777d29e536e20d8257940ce48afc621b1d7f
fibo <casati_gianluca@yahoo.it>
2018-10-10 23:42:35 +0200

    updated dev deps

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ const volatility = require('./index')
 const values = [727.7, 1086.5, 1091.0, 1361.3, 1490.5, 1956.1]
 
 const sigma = volatility(values)
-assert.equal(sigma, 384.2844190469114)
+assert.strictEqual(sigma, 384.2844190469114)
 
 console.log(`result is ${sigma}`)
